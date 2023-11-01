13e3f50339d28ef980e2d989d0f4bfc736da99df
Intetras <77957619+Intetras@users.noreply.github.com>
2023-11-01 10:34:37 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 //% weight=30 icon="\uf1b9" color=#000080 block="アイデオキット"
 namespace kagabitideo {
 
+    //% group="ロボットカー制御"
+    //% blockId="ideoTest"
+    //% block="テスト"
     export function　ideoTest(){
-        
     }
+
 }
