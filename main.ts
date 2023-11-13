f17b78f4d7e585e15c2da4b3441866132e3ba22d
Intetras <77957619+Intetras@users.noreply.github.com>
2023-11-13 12:21:27 +0900

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,18 @@
 //% weight=30 icon="\uf1b9" color=#000080 block="アイデオキット"
 namespace kagabitideo {
 
-    //% group="ロボットカー制御"
-    //% blockId="ideoTest"
+    //% group="リレー制御"
+    //% blockId="relay_on"
     //% block="テスト"
-    export function　ideoTest(){
+    export function　relayOn(){
+        pins.digitalWritePin(DigitalPin.P8, 1)
+    }
+
+    //% group="リレー制御"
+    //% blockId="relay_on"
+    //% block="テスト"
+    export function relayOff() {
+        pins.digitalWritePin(DigitalPin.P8, 0)
     }
 
 }
