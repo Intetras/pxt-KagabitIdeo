694b0ae7a151fa874ca4c2f9c2edbce9e7584ed3
Intetras <77957619+Intetras@users.noreply.github.com>
2023-11-15 16:50:09 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 //% weight=30 icon="\uf1b9" color=#000080 block="アイデオキット"
 namespace kagabitideo {
 
+    export enum direction {
+        //% block="前"
+        Forward = 1,
+        //% block="後"
+        Back = 0
+    }
+
     //% group="リレー制御"
     //% blockId="relay_on"
     //% block="リレー ON"
@@ -13,6 +20,29 @@ namespace kagabitideo {
     //% block="リレー OFF"
     export function relayOff() {
         pins.digitalWritePin(DigitalPin.P8, 0)
+    }
+
+    //% group="モーター制御"
+    //% blockId="motor"
+    //% block="モーターの強さ %power"
+    export function motorA(power: number) {
+        if (power > 1023) {
+            power = 1023
+        } else if (power < -1023) {
+            power = -1023
+        }
+
+        if (power > 0) {
+            pins.digitalWritePin(DigitalPin.P13, direction.Forward);
+            pins.analogWritePin(AnalogPin.P14, Math.abs(power));
+        } else if (power < 0) {
+            pins.digitalWritePin(DigitalPin.P13, direction.Back);
+            pins.analogWritePin(AnalogPin.P14, Math.abs(power));
+        } else {
+
+            pins.analogWritePin(AnalogPin.P14, 0);
+
+        }
     }
 
 }
